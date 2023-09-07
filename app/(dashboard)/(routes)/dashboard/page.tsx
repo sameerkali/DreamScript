de52a5b75b3fb5a731d3c0d41ce143f47ffd22ea
Sameer Faridi <s.faridi007@gmail.com>
2023-09-07 16:57:07 +0530

    Ui of Conversation page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,7 +4,9 @@ import { cn } from "@/lib/utils";
 import { ArrowRight, MessageSquare } from "lucide-react";
 
 import { tools } from "@/constants";
+import { useRouter } from "next/navigation";
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -17,7 +19,7 @@ const DashboardPage = () => {
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map(tool =>
-          <Card onClick={() => {}} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
           <div className="flex items-center gap-x-4">
             <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
               <tool.icon className={cn("w-8 h-8", tool.color)} />
