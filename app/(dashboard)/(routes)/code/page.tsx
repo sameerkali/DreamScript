cd502b9054a27f24a897b235671a5dc1b2968173
Sameer Faridi <s.faridi007@gmail.com>
2023-09-12 08:21:12 +0530

    Change route ( not working )

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -32,19 +32,16 @@ const CodePage = () => {
     },
   });
 
+ 
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try{
         const userMessage: OpenAI.Chat.CreateChatCompletionRequestMessage = { role: "user", content: values.prompt };
         const newMessages = [...messages, userMessage];
-        const response = await axios.post('/api/code', { messages: newMessages });
-        console.log("ye hamara response ka data hai bhai =================="+response.data)
-        // /app/api/conversation/rout.ts
+        const response = await axios.post("/app/api/code", { messages: newMessages });
+        // console.log("ye hamara response ka data hai bhai =================="+response.data)
       setMessages((current) => [...current, userMessage, response.data]);
       form.reset()
-
-
-
     } catch(error: any){
       console.log("ye hamara error hai bhai =================="+error)
     }finally {
@@ -142,128 +139,3 @@ const CodePage = () => {
 
 export default CodePage;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   <div>
-  //     <Heading
-  //       title="Code Generation"
-  //       description="Our most advanced conversation model."
-  //       icon={MessageSquare}
-  //       iconColor="text-viollet-500"
-  //       bgColor="bg-violet-500/10"
-  //     />
-  //     <div className="px-4 lg:px-8">
-  //       <div>
-  //         <Form {...form}>
-  //           <form
-  //             onSubmit={form.handleSubmit(onSubmit)}
-  //             className="
-  //               rounded-lg 
-  //               border 
-  //               w-full 
-  //               p-4 
-  //               px-3 
-  //               md:px-6 
-  //               focus-within:shadow-sm
-  //               grid
-  //               grid-cols-12
-  //               gap-2
-  //             "
-  //           >
-  //             <FormField
-  //               name="prompt"
-  //               render={({ field }) => (
-  //                 <FormItem className="col-span-12 lg:col-span-10">
-  //                   <FormControl className="m-0 p-0">
-  //                     <Input
-  //                       className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-  //                       placeholder="Write queries to create it that can be scaled to millions of users."
-  //                       disabled={isLoading}
-  //                       {...field}
-  //                     />
-  //                   </FormControl>
-  //                 </FormItem>
-  //               )}
-  //             />
-  //             <Button
-  //               className="col-span-12 lg:col-span-2 w-full"
-  //               disabled={isLoading}
-  //             >
-  //               Generate
-  //             </Button>
-  //           </form>
-  //         </Form>
-  //       </div>
-  //       <div className="space-y-4 mt-4">
-  //         {isLoading && (
-  //           <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-  //           <Loader />
-  //         </div>
-  //         )}
-  //       {messages.length === 0 && !isLoading && (<Empty label="No Conversation Started" />)}
-  //         <div className="flex flex-col-reverse gap-y-4">
-  //             {messages.map((message) => (
-  //               <div
-  //                key={message.content}
-  //                className={cn(
-  //                 "p-8 w-full flex items-start gap-x-8 rounded-lg",
-  //                 message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
-  //               )}
-  //                >
-  //                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-  //               <p className="text-sm">
-  //                 {message.content}
-  //               </p>
-  //               </div>
-  //             ))}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
